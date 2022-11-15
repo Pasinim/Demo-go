34d6f37934ef7c70ad726c87dc169b385ab9ebc2
marco.pasini@studenti.unimi.it <marco.pasini@studenti.unimi.it>
2022-11-15 10:47:15 +0100

    .

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"demo/api"
+	"demo/pg_repo"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
+	"net/http"
 )
 
-//
-//func main() {
-//	r := pg_repo.New()
-//	a := api.New(r)
-//	srv := http.Server{
-//		Addr:    ":8080",
-//		Handler: a,
-//	}
-//	srv.ListenAndServe()
-//}
+func main() {
+	r := pg_repo.New()
+	a := api.New(r)
+	srv := http.Server{
+		Addr:    ":8080",
+		Handler: a,
+	}
+	srv.ListenAndServe()
+}
